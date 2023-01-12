7b4ab7476017b33741d9d9914d55c8fa936d2242
Jay-J-Williams <101154320+Jay-J-Williams@users.noreply.github.com>
2023-01-12 17:03:47 +1100

    Booking Page Updates
    - Added Header
    - Added Questions for the form
    - Added Footer

--- a2/booking.php
+++ a2/booking.php
@@ -14,11 +14,15 @@
   <body>
 
     <header>
-      <div>Put company logo and name here</div>
+      <div>Lunardo <img src='../../media/Lunardo-Logo.png' alt='Lunardo Cinema Logo', width='100', height='100'/> Cinema</div>
     </header>
 
     <nav>
-      <div>Put website navigation links here</div>
+      <div>        
+        <ul>
+            <li><a href='index.php'>HOME</a>
+        </ul>
+      </div>
     </nav>
 
     <main>
@@ -26,11 +30,35 @@
     <!-- Creative Commons image sourced from https://pixabay.com/en/maintenance-under-construction-2422173/ and used for educational purposes only -->
         <img src='../../media/website-under-construction.png' alt='Website Under Construction' />
       </article>
+      <form action='booking.php' method='get'>
+          <p>Choose a Movie:</p>
+          <input type='radio' id='avatar' name='MovieChoice' value='Avatar>
+          <label for='avatar'>Avatar: The Way of Water</label><br>
+          <input type='radio' id='weird' name='MovieChoice' value='Weird'>
+          <label for='weird'>Weird: The Al Yankovic Story</label><br>
+          <input type='radio' id='puss_in_boots' name='MovieChoice' value='PussInBoots'>
+          <label for='puss_in_boots'>Puss in Boots: The Last Wish</label><br>
+          <input type='radio' id='margrete' name='MovieChoice' value='Margrete'>
+          <label for='margrete'>Margrete: Queen of the North</label><br>
+
+          <br>
+          
+          <select name='SeatGroup' id='seatgroup'>
+            <option value='select' disabled>Select a Seat Group</option>
+            <option value='standard_adult[STA]'>Standard Adult</option>
+            <option value='standard_concession[STP]'>Standard Concession</option>
+            <option value='standard_child[STC]'>Standard Child</option>
+            <option value='first_class_adult[FCA]'>First Class Adult</option>
+            <option value='first_class_concession[FCP]'>First Class Concession</option>
+            <option value='first_class_child[FCC]'>First Class Child</option>
+          </select>
+          <input type='submit' value="Add">
+      </form>
     </main>
     <footer>
       <div>&copy;<script>
         document.write(new Date().getFullYear());
-      </script> Put your name(s), student number(s) and group name here. Last modified <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>.</div>
+      </script> Email: LunardoCinema@gmail.com, Phone: 555 555 555, Address: 2 Smith Road, Smithville.  Jay Meredith, S3951987, <a href=https://github.com/Jay-J-Williams/wp>GitHub Link</a>. Last modified <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>.</div>
       <div>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.</div>
       <div><button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button></div>
     </footer>
